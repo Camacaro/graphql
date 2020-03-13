393b78c0090cec1a8f259726dae57014a3be9f3a
jcamacaro <jesus.alejandr@gmail.com>
2020-03-12 22:54:35 -0300

    agregando cors, script para production, clients grapql

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const { makeExecutableSchema } = require('graphql-tools');
 // const { buildSchema } = require('graphql');
 const express = require('express');
+const cors = require('cors');
 const gqlMiddleware = require('express-graphql');
 const { readFileSync } = require('fs');
 const { join } = require('path')
@@ -15,6 +16,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express();
 const port = process.env.port || 3000; 
+const isDev = process.env.NODE_ENV !== 'production';
 
 // definimos un schema 
 // Integer, boolean, String, floaHola Mundot
@@ -32,11 +34,12 @@ const typeDefs = readFileSync(
 
 const schema = makeExecutableSchema({typeDefs, resolvers});
 
+app.use( cors() );
 
 app.use('/api', gqlMiddleware({
     schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
 }));
 
 app.listen(port, () => {
